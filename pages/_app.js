3f040aa70fb0ac351198aebd3a8f20ac96ce6d07
LuisLamiral8 <luislamiraal@gmail.com>
2022-02-08 22:42:32 -0300

    log in wallet progress

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.scss'
 import '../styles/staking.scss'
+import '../styles/connectpage.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,30 @@
-
+import Image from 'next/image'
+import Metamask from '../public/images/wallets/metamask.png'
 
 export default function Home() {
   return (
-<form action="" id="container-form">
-      <div className="info">
-      </div>
-      <div id="connect" className="metamask">
-        {/* <img
+    <form action="" id="container-form">
+      <div className="connect-container">
+        <div className="info">
+          <h1>Log in with wallet</h1>
+        </div>
+        <button id="connect" className="btn metamask">
+          {/* <img
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/MetaMask_Fox.svg/1200px-MetaMask_Fox.svg.png"
-          alt="metamask logo" width="70px" height="70px"> */}
-        <h1>Connect with metamask</h1>
-      </div>
-      <div id="connect" className="metamask">
-        {/* <img src="https://trustwallet.com/assets/images/media/assets/trust_platform.svg" alt="trustwallet logo"
+        alt="metamask logo" width="70px" height="70px"> */}
+          <Image className="btn-ico" src={Metamask} width="150px" height="75px" alt=''/>
+          <h2>Connect with metamask</h2>
+        </button>
+        <button id="connect" className="btn trustwallet">
+          {/* <img src="https://trustwallet.com/assets/images/media/assets/trust_platform.svg" alt="trustwallet logo"
           width="70px" height="70px"> */}
-        <h1>Connect with Trustwallet</h1>
-      </div>
-      <div id="connect" className="metamask">
-        {/* <img src="https://help.coinbase.com/public-assets/favicons/apple-icon-180x180.png" alt="trustwallet logo"
+          <h2>Connect with Trustwallet</h2>
+        </button>
+        <button id="connect" className="btn coinbase">
+          {/* <img src="https://help.coinbase.com/public-assets/favicons/apple-icon-180x180.png" alt="trustwallet logo"
           width="70px" height="70px"> */}
-        <h1>Connect with CoinBase</h1>
+          <h2>Connect with CoinBase</h2>
+        </button>
       </div>
     </form>
   )
